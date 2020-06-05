2e0f5496d1b9d6f88c0acebfd24b98b94b703b9a
turabhaiderkazmi <turabhaiderkazmi@gmail.com>
2020-06-05 22:30:56 +0500

    Assignment 2 CacheMatrix Final

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The R functions in this script take a square matrix, cache its data
+## and its inverse in a seperate function.
 
-## Write a short comment describing this function
+## The 'makeCacheMatrix' function takes a square matrix as an input
+## and creates a list can cache the inverse of the input matrix into a 
+## special 'matrix' (class=list). 
+## We set and get value of the matrix and its inverse in this function.
 
 makeCacheMatrix <- function(x = matrix()) {
 	  m <- NULL
@@ -18,7 +21,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The 'cacheSolve' function calculates the inverse of the special matrix
+## created in the 'makeCacheMatrix' function. It checks if the inverse is 
+## already calculated or not. If it is, the function will return the already
+## calculated inverse otherwise it will use Solve() function to calculate the 
+## inverse of the matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
